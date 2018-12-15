52635c3e88381894bdfb7441363d484818572bb4
Alex <alexandre.cruel@orange.fr>
2018-12-15 15:04:30 +0100

    auto stéréotype w/ dbscan

--- test.py
+++ test.py
@@ -5,15 +5,13 @@ trad = Translator(from_lang='fr', to_lang="en")
 
 model = KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True)
 
-condSortie = 1
+#condSortie = 1
 
-while condSortie != 0:
-    myInput = input("Entrez le mot à trouver : ")
+#while condSortie != 0:
+myInput = input("Entrez le mot à trouver : ")
 
-    word = trad.translate(myInput)
-    # word = 'Macron'
-    print('# of words', len(model.wv.vocab))
-    print('sample words', list(model.wv.vocab.keys())[:10])
+word = trad.translate(myInput)
+print('# of words', len(model.wv.vocab))
+print('sample words', list(model.wv.vocab.keys())[:10])
 
-    print(f'most similar words to {word} :\n{model.most_similar(word)}')
-    #print(f'vector of word :\n{model[word]}')
+print(f'most similar words to {word} :\n{model.most_similar(word)}')
